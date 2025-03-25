cf57fdde9d333189829eb536be4d4b07fb31a943
Eros De Simone <eros.desimone@yahoo.it>
2025-03-25 14:11:16 +0100

    localhost invece di 19

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // Configurazione base di axios
-const API_URL = 'http://192.168.1.19:8000';  // Sostituisci con l'IP del tuo computer
+const API_URL = 'http://localhost:8000';  // Sostituisci con l'IP del tuo computer
 
 // Crea un'istanza di axios
 const api = axios.create({
