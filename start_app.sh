382da6d2d64f18ca341bb1fa36e1cd8dc7289f4d
Eros De Simone <eros.desimone@yahoo.it>
2025-03-26 19:50:43 +0100

    gitignore e start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -10,22 +10,15 @@ cleanup() {
 trap cleanup SIGINT SIGTERM
 
 # Avvia backend FastAPI
-cd /Users/erosdesimone/VSProject/feedyourmind/backend
-source env/bin/activate
+cd /Users/erosdesimone/VSProject/feedyourmindapp/backend
+source venv/bin/activate
 echo "Avvio del backend su http://localhost:8000..."
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 # Avvia frontend React
-cd /Users/erosdesimone/VSProject/feedyourmind/frontend
+cd /Users/erosdesimone/VSProject/feedyourmindapp/frontend
 echo "Avvio del frontend su http://localhost:3000..."
 npm start -- --host 0.0.0.0 &
-
-# Attendi che i servizi siano pronti
-sleep 5
-
-# Esponi Nginx con ngrok (porta 80)
-echo "Avvio tunnel ngrok verso Nginx..."
-ngrok http 80 --host-header=rewrite
 
 # Attendi l'interruzione
 wait
